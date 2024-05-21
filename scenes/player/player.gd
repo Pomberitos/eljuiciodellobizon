fd2fbd95d5970a0208252273453800a687428148
Gustavo León <gleondiaz@gmail.com>
2024-05-21 11:21:20 -0400

    proto WIP

---
+++ scenes/player/player.gd
@@ -0,0 +1,24 @@
+class_name Player
+extends CharacterBody2D
+
+@export var max_speed = 100.0
+@export var accel = 1000.0
+@export var friction = 500.0
+
+@export var lamp: PointLight2D
+var _is_lamp_active: bool = false
+
+
+func _physics_process(delta: float) -> void:
+	var input = Input.get_vector("left", "right", "up", "down")
+	if input.length() > 0:
+		velocity = velocity.move_toward(input * max_speed, accel * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	move_and_slide()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("lamp"):
+		_is_lamp_active = !_is_lamp_active
+		lamp.enabled = _is_lamp_active
---
+++ scenes/slasher/slasher.gd
@@ -0,0 +1,18 @@
+class_name Slasher
+extends CharacterBody2D
+
+@export_range(20, 45, 1, "degrees") var angle_degrees
+@export_range(10, 50, 1) var max_view_distance
+@export_range(5, 10, 1, "degrees") var angle_between_rays
+
+
+
+func generate_raycast()->void:
+	var ray_count:float = deg_to_rad(angle_degrees) / deg_to_rad(angle_between_rays)
+	
+	for ray_index in ray_count:
+		var ray: RayCast2D = RayCast2D.new()
+		var angle: float =  deg_to_rad(angle_degrees) * (ray_index - ray_count/2.0)
+		ray.target_position = Vector2.UP.rotated(angle) * max_view_distance
+		add_child(ray)
+		ray.enabled
