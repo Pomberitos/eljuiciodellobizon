cd885132446886beece76b1c11c392db28156267
Pablo Terradillos <tehsis@gmail.com>
2024-06-02 10:03:10 -0300

    Pathfinding agregado al slasher

--- scenes/slasher/slasher.gd
+++ scenes/slasher/slasher.gd
@@ -1,18 +1,18 @@
 class_name Slasher
 extends CharacterBody2D
 
-@export_range(20, 45, 1, "degrees") var angle_degrees
-@export_range(10, 50, 1) var max_view_distance
-@export_range(5, 10, 1, "degrees") var angle_between_rays
+const speed = 200
 
+@export var player: Node2D
+@onready var nav_agent = $NavigationAgent2D as NavigationAgent2D
 
+func _physics_process(_delta: float) -> void:
+    var dir = to_local(nav_agent.get_next_path_position()).normalized()
+    velocity = dir * speed
+    move_and_slide()
 
-func generate_raycast()->void:
-	var ray_count:float = deg_to_rad(angle_degrees) / deg_to_rad(angle_between_rays)
-	
-	for ray_index in ray_count:
-		var ray: RayCast2D = RayCast2D.new()
-		var angle: float =  deg_to_rad(angle_degrees) * (ray_index - ray_count/2.0)
-		ray.target_position = Vector2.UP.rotated(angle) * max_view_distance
-		add_child(ray)
-		ray.enabled
+func _on_timer_timeout():
+    makepath()
+
+func makepath() -> void:
+    nav_agent.target_position = player.global_position
