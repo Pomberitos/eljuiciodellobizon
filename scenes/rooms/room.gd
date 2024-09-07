8b6ddede28b406c2598b1f33193dcc9de21e6737
Pablo Terradillos <tehsis@gmail.com>
2024-09-06 22:47:41 -0300

    Removidos prints

--- scenes/rooms/room.gd
+++ scenes/rooms/room.gd
@@ -10,5 +10,4 @@ func _ready():
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("Player Entered Room: " + str(self.name))
 		Events.room_entered.emit(self)
